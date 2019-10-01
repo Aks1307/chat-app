e7a88830329368b1c01f95c0194b0b44053d0c1c
Anshu Kumar Singh <40080782+Aks1307@users.noreply.github.com>
2019-10-01 20:25:20 +0530

    Add files via upload

---
+++ public/js/bot.js
@@ -0,0 +1,62 @@
+var questions = [
+    'Whats your name ?',
+    'Where are you from?',
+    'What\'s your age?',
+    'What profile you are working on?',
+    'Join Chat room "Admin" for further Queries'
+    
+];
+var num = 0;
+
+var inputBox = document.querySelector("#ans");
+var output = document.querySelector("#result");
+output.innerHTML = questions[num];
+
+function showResponse() {
+  var input = inputBox.value;
+  if(inputBox.value == "") {
+    
+  }else {
+  if(num == 0) {
+    output.innerHTML = `Hii ${input}`;
+    inputBox.value = "";
+    
+    ++num;
+    setTimeout(changeQuestion, 1000);
+  } else if(num == 1) {
+    output.innerHTML = `${input} must be a good place`;
+    inputBox.value = "";
+    
+    ++num;
+    setTimeout(changeQuestion, 1000);
+  } else if(num == 2) {
+    output.innerHTML = `So you are ${2019 - input} born`;
+    inputBox.value = "";
+    
+    ++num;
+    setTimeout(changeQuestion, 1000);
+  } else if(num == 3) {
+    output.innerHTML = `Awesome ${input}`;
+    inputBox.value = "";
+    
+    ++num;
+    setTimeout(changeQuestion, 1000);
+  }
+  }
+}
+
+function changeQuestion() {
+  inputBox.setAttribute("placeholder", "Enter your response");
+  output.innerHTML = questions[num];
+  if(num == 4) {
+    inputBox.style.display = "none";
+  }
+}
+
+$(document).on('keypress', function(e) {
+  if(e.which == 13) {
+    showResponse();
+  }
+})
+
+$( "#ans" ).focus();
